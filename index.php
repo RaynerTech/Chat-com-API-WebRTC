237f51d73e6a4909c794eed388be305584f29dd5
Rayner Dev <RaynerPatrick@gmail.com>
2020-10-01 20:22:02 -0300

    Primeira parte do Projeto Finalizado

--- index.php
+++ index.php
@@ -1,181 +1,220 @@
 <!DOCTYPE html>
 <html lang="pt">
+
 <head>
-<title>Vídeo Chat</title>
+    <title>Vídeo Chat</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src='https://cdn.scaledrone.com/scaledrone.min.js' type='text/javascript'></script>
 
     <style>
-        
-       
-
-        .sepa{
-            width:100%;
-            display:flex;
-            height:100vh;
-            margin:0;
-            padding:0;
+        .sepa {
+            width: 100%;
+            display: flex;
+            height: 100vh;
+            margin: 0;
+            padding: 0;
             align-items: center;
-            justify-content:center;
-            
+            justify-content: center;
+
         }
 
 
 
-        video{
-            width:80%;
-            box-sizing:border-box;
-            margin:0 50px;
-            border-radius:2px;
-            padding:0;
-            border:1px solid black;      
-            
-            
+        video {
+            width: 80%;
+            box-sizing: border-box;
+            margin: 0 50px;
+            border-radius: 2px;
+            padding: 0;
+            border: 1px solid black;
+
+
+        }
+
+        .bem {
+            position: fixed;
+            text-align: center;
+            top: 10px;
+            left: 50px;
+            transform: translate(-50%, -50%)
+        }
+
+        header {
+            text-align: center;
+            color: red;
+            font-size: 30px;
+            background: blue;
+            margin: 20px 0;
+            padding: 10px;
+        }
+
+        @media (max-width: 600px) {
+            video {
+                max-width: calc(50% - 70px);
             }
-
-            .bem{
-                position:fixed;
-                text-align: center;
-                top:10px;
-                left:50px;
-                transform: translate(-50%, -50%)
-            }
-
-            header{
-                text-align: center;
-                color:red;
-                font-size:30px;
-                background:blue;
-                margin: 20px 0;
-                padding: 10px;
-            }
-
-            @media (max-width: 600px)
-            {
-                video{
-                   max-width:calc(50% - 70px);
-                }
-            } 
-
-
+        }
     </style>
 </head>
+
 <body>
-    
-        <header>
-            Nosso Chat
-        </header>
-   
 
-  <div class="sepa">
-        <video id="local" autoplay muted></video>
-        <video id="remove" autoplay></video>
-        </div>
+    <header>
+        Nosso Chat
+    </header>
 
 
-<script>
-    
-if(!location.hash){
-    location.hash = Math.floor(Math.random() * 0xFFFFFF).toString(16)
+    <div class="sepa">
+        <video id="local" autoplay></video>
+        <video id="remoteVideo" autoplay></video>
+    </div>
 
-}
-
-const roomHash = location.hash.substring(1)
-
-/*Channel ID GERADO NO SITE https://dashboard.scaledrone.com/*/
-const drone = new ScaleDrone('####');
-
-const roomName = 'observable-'+roomHash
-
-/*Servidor de conecçãoj*/
-const configuration = {
-    iceServers:[
-        {
-            urls: 'stun:stun.l.google.com:19302'
+    <script>
+        //Início ScaleDrone e WebRTC
+        if (!location.hash) {
+            location.hash = Math.floor(Math.random() * 0xFFFFFF).toString(16);
         }
-    ]
-}
 
-let room
-let pc
+        const roomHash = location.hash.substring(1);
+        // O channel ID pode pegar no site: https://dashboard.scaledrone.com/
+        const drone = new ScaleDrone('TzYGeQtKdHe82oOz');
 
-let number = 0
+        const roomName = 'observable-' + roomHash;
 
-function OnSuccess(){}
+        const configuration = {
 
-function onError(){
-    console.log(error)
+            iceServers: [
 
-}
+                {
+                    urls: 'stun:stun.l.google.com:19302'
+                }
 
-/*Tratamento de erro*/
-drone.on('open',error => {
-    if(error)
-        return console.log(error)
-    room = drone.subscribe(roomName)
+            ]
 
-    room.on('open', error =>{
+        }
 
-    })
+        let room;
+        let pc;
 
-/**teste do web socket */
-    room.on('members', members=>{
-        console.log("Conectado!")
-
-        console.log("conexões abertas " + members.length)
-  /**Validação controle de usuarios  */      
-        number = members.length - 1
-        const isOfferer = members.length >= 2
-
-        startWebRTC(isOfferer)
-   
-    })
-/** */
-  
-})
+        let number = 0;
 
 
-function sendMessage(message){
-        drone.publish({
-            room: roomName,message
-        })
-    }
-/**detectando os eventos */
-    function startWebRTC(isOfferer){
-        pc = new RTCPeerConnection(configuration)
+        function onSuccess() {};
 
-        pc.onicecandidate = event =>{
-            if(event.candidate){
-                sedMessage({'candidate':event.candidate})
+        function onError(error) {
+            console.log(error);
+        };
+
+/// teste do web socket 
+        drone.on('open', error => {
+            if (error)
+                return console.log(error);
+
+            room = drone.subscribe(roomName);
+
+
+            room.on('open', error => {
+                //Erro capturado!
+
+            });
+//teste do web socket 
+            room.on('members', members => {
+
+                console.log("Conectado!");
+
+                console.log("Conexões abertas: " + members.length);
+                number = members.length - 1;
+                const isOfferer = members.length >= 2;
+
+                startWebRTC(isOfferer);
+
+            })
+
+        });
+
+        function sendMessage(message) {
+            drone.publish({
+                room: roomName,
+                message
+            })
+        }
+
+
+        function startWebRTC(isOfferer) {
+
+
+            pc = new RTCPeerConnection(configuration);
+
+            pc.onicecandidate = event => {
+                if (event.candidate) {
+                    sendMessage({
+                        'candidate': event.candidate
+                    });
+                }
             }
-        }
 
-        if(isOfferer){
-            pc.onnegotiationneeded = () =>{
-                pc.createOffer().then(localDescCreated).catch(onError)
 
+            if (isOfferer) {
+                pc.onnegotiationneeded = () => {
+                    pc.createOffer().then(localDescCreated).catch(onError)
+                }
             }
-        }
 
-        pc.ontrack = event =>{
-            const stream = event.streams[0]
 
-            if(!remote.srcObject || remote.srtObject.id !== stream.id){
-                remote.srcObject = stream
+
+            pc.ontrack = event => {
+                const stream = event.streams[0];
+
+
+                if (!remoteVideo.srcObject || remoteVideo.srcObject.id !== stream.id) {
+                    remoteVideo.srcObject = stream;
+                }
             }
+
+
+            navigator.mediaDevices.getUserMedia({
+                audio: true,
+                video: true,
+            }).then(stream => {
+                local.srcObject = stream;
+                stream.getTracks().forEach(track => pc.addTrack(track, stream))
+            }, onError)
+            //SAIDA DO USUARI
+            room.on('member_leave', function(member) {
+                
+                remoteVideo.style.display = "none";
+            })
+
+            room.on('data', (message, client) => {
+
+                if (client.id === drone.clientId) {
+                    return;
+                }
+
+                if (message.sdp) {
+                    pc.setRemoteDescription(new RTCSessionDescription(message.sdp), () => {
+                        if (pc.remoteDescription.type === 'offer') {
+                            pc.createAnswer().then(localDescCreated).catch(onErrror);
+                        }
+                    }, onError)
+                } else if (message.candidate) {
+                    pc.addIceCandidate(
+                        new RTCIceCandidate(message.candidate), onSuccess, onError)
+                }
+
+            })
+
         }
 
-        navigator.mediaDevices.getUserMedia({
-            audio: true,
-            video: true
-        }).then(stream =>{
-            local.srcObject = stream
-            stream.getTracks().forEach(track=>pc.addTrack(track,stream))
-        },onError)
-         
-    }
+        function localDescCreated(desc) {
+            pc.setLocalDescription(
+                desc, () => sendMessage({
+                    'sdp': pc.localDescription
+                }), onError
+            )
+        }
+    </script>
 
-</script>
 </body>
+
 </html>
